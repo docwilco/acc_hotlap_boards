261ac5a37b951278558b93211568f8d9c38748af
DocWilco <github@drwil.co>
2024-04-25 15:07:48 +0000

    Actual pretty HTML, more data

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 use anyhow::{anyhow, Context, Result};
 use askama_axum::Template;
 use axum::{response::IntoResponse, routing::get, Router};
+use chrono::{Date, DateTime, Local, NaiveDate, NaiveDateTime, Utc};
 use itertools::Itertools;
+use phf::{phf_map, Map};
 use serde::Deserialize;
 use serde_json::Value;
 use serde_with::{serde_as, DurationMilliSeconds};
@@ -14,15 +16,91 @@ use std::{
     time::Duration,
 };
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 
-type DriverLaps = HashMap<String, Vec<Duration>>;
+struct Lap {
+    laptime: Duration,
+    car: u64,
+    date: DateTime<Utc>,
+    splits: Vec<Duration>,
+}
+
+type DriverLaps = HashMap<String, Lap>;
 //type Data = HashMap<String, DriverLaps>;
 struct Data {
     by_track: HashMap<String, DriverLaps>,
     player_id_to_name: HashMap<String, String>,
 }
 
-type DisplayData = Vec<(String, Vec<(String, String)>)>;
+static CAR_MODEL_ID_TO_NAME: Map<u64, &'static str> = phf_map! {
+    0_u64 => "Porsche 991 GT3 R",
+    1_u64 => "Mercedes-AMG GT3",
+    2_u64 => "Ferrari 488 GT3",
+    3_u64 => "Audi R8 LMS",
+    4_u64 => "Lamborghini Huracán GT3",
+    5_u64 => "McLaren 650S GT3",
+    6_u64 => "Nissan GT-R Nismo GT3",
+    7_u64 => "BMW M6 GT3",
+    8_u64 => "Bentley Continental GT3",
+    9_u64 => "Porsche 991 II GT3 Cup",
+    10_u64 => "Nissan GT-R Nismo GT3",
+    11_u64 => "Bentley Continental GT3",
+    12_u64 => "AMR V12 Vantage GT3",
+    13_u64 => "Reiter Engineering R-EX GT3",
+    14_u64 => "Emil Frey Jaguar G3",
+    15_u64 => "Lexus RC F GT3",
+    16_u64 => "Lamborghini Huracan GT3 Evo",
+    17_u64 => "Honda NSX GT3",
+    18_u64 => "Lamborghini Huracan SuperTrofeo",
+    19_u64 => "Audi R8 LMS Evo",
+    20_u64 => "AMR V8 Vantage",
+    21_u64 => "Honda NSX GT3 Evo",
+    22_u64 => "McLaren 720S GT3",
+    23_u64 => "Porsche 991 II GT3 R",
+    24_u64 => "Ferrari 488 GT3 Evo",
+    25_u64 => "Mercedes-AMG GT3",
+    26_u64 => "Ferrari 488 Challenge Evo",
+    27_u64 => "BMW M2 Club Sport Racing",
+    28_u64 => "Porsche 992 GT3 Cup",
+    29_u64 => "Lamborghini Huracán SuperTrofeo EVO2",
+    30_u64 => "BMW M4 GT3",
+    31_u64 => "Audi R8 LMS GT3 Evo 2",
+    32_u64 => "Ferrari 296 GT3",
+    33_u64 => "Lamborghini Huracan GT3 Evo 2",
+    34_u64 => "Porsche 992 GT3 R",
+    35_u64 => "McLaren 720S GT3 Evo",
+    50_u64 => "Alpine A110 GT4",
+    51_u64 => "Aston Martin Vantage GT4",
+    52_u64 => "Audi R8 LMS GT4",
+    53_u64 => "BMW M4 GT4",
+    55_u64 => "Chevrolet Camaro GT4",
+    56_u64 => "Ginetta G55 GT4",
+    57_u64 => "KTM X-Bow GT4",
+    58_u64 => "Maserati MC GT4",
+    59_u64 => "McLaren 570S GT4",
+    60_u64 => "Mercedes AMG GT4",
+    61_u64 => "Porsche 718 Cayman GT4 Clubsport",
+    80_u64 => "Audi R8 LMS GT2",
+    82_u64 => "KTM XBOW GT2",
+    83_u64 => "Maserati MC20 GT2",
+    84_u64 => "Mercedes AMG GT2",
+    85_u64 => "Porsche 911 GT2 RS CS Evo",
+    86_u64 => "Porsche 935",
+};
+
+struct DisplayLine {
+    player_id: String,
+    position: usize,
+    name: String,
+    laptime: String,
+    gap: String,
+    interval: String,
+    splits: String,
+    car: String,
+    date: i64,
+}
+
+type DisplayData = Vec<(String, Vec<DisplayLine>)>;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -202,7 +280,11 @@ fn bytes_to_json_string(bytes: &[u8]) -> Result<String> {
     Err(anyhow::anyhow!("Failed to figure out JSON file encoding"))
 }
 
-fn add_session_results(data: &mut Data, session_results: JsonSessionResults) {
+fn add_session_results(
+    data: &mut Data,
+    session_results: JsonSessionResults,
+    session_datetime: DateTime<Utc>,
+) -> Result<()> {
     let valid_laps = session_results
         .laps
         .iter()
@@ -215,34 +297,43 @@ fn add_session_results(data: &mut Data, session_results: JsonSessionResults) {
             .entry(track_name)
             .or_insert(Default::default());
         for lap in valid_laps {
-            let driver = session_results
+            let car = session_results
                 .session_result
                 .leader_board_lines
                 .iter()
                 .find_map(|line| {
                     if line.car.car_id == lap.car_id {
-                        let driver = &line.car.drivers[lap.driver_index as usize];
-                        Some(driver)
+                        Some(&line.car)
                     } else {
                         None
                     }
-                });
-            if let Some(driver) = driver {
-                let driver_entry = driver_laps
-                    .entry(driver.player_id.clone())
-                    .or_insert(Default::default());
-                driver_entry.push(lap.laptime);
-                driver_entry.sort();
-                data.player_id_to_name.insert(
-                    driver.player_id.clone(),
-                    format!(
-                        "{} {} ({})",
-                        driver.first_name, driver.last_name, driver.short_name
-                    ),
-                );
+                })
+                .context(anyhow!("Failed to find car for lap"))?;
+            let driver = car
+                .drivers
+                .get(lap.driver_index as usize)
+                .context(anyhow!("Failed to find driver for lap"))?;
+            if driver_laps.get(&driver.player_id).map_or(false, |fastest| lap.laptime > fastest.laptime)
+            {
+                continue;
             }
+            let splits = lap.splits.clone();
+            driver_laps.insert(driver.player_id.clone(), Lap {
+                laptime: lap.laptime,
+                car: car.car_model,
+                date: session_datetime,
+                splits,
+            });
+            data.player_id_to_name.insert(
+                driver.player_id.clone(),
+                format!(
+                    "{} {} ({})",
+                    driver.first_name, driver.last_name, driver.short_name
+                ),
+            );
         }
     }
+    Ok(())
 }
 
 fn read_file(path: impl AsRef<Path>) -> Result<JsonSessionResults> {
@@ -280,8 +371,15 @@ fn compute_data(results_dir: impl AsRef<Path>) -> Result<Data> {
         {
             continue;
         }
+        // Parse filename to get DateTime
+        let session_datetime = NaiveDateTime::parse_from_str(&file_name[0..13], "%y%m%d_%H%M%S")
+            .context(anyhow!("Failed to parse datetime from filename"))?
+            .and_local_timezone(Local)
+            .earliest()
+            .context(anyhow!("Failed to convert datetime to local timezone"))?
+            .with_timezone(&Utc);
         let session_results = read_file(&path)?;
-        add_session_results(&mut data, session_results);
+        add_session_results(&mut data, session_results, session_datetime)?;
     }
 
     Ok(data)
@@ -293,6 +391,18 @@ struct RootTemplate {
     display_data: DisplayData,
 }
 
+fn format_duration(duration: Duration) -> String {
+    let seconds = duration.as_secs();
+    let minutes = seconds / 60;
+    let seconds = seconds % 60;
+    let milliseconds = duration.subsec_millis();
+    if minutes == 0 {
+        format!("{}.{:03}", seconds, milliseconds)
+    } else {
+        format!("{}:{:02}.{:03}", minutes, seconds, milliseconds)
+    }
+}
+
 async fn root() -> impl IntoResponse {
     let data = compute_data("/home/ac/accsm/server/results").unwrap();
     let display_data = data
@@ -300,17 +410,55 @@ async fn root() -> impl IntoResponse {
         .into_iter()
         .sorted_by_key(|(track, _)| track.to_string())
         .map(|(track, driver_laps)| {
+            // Replace _ with space, and capitalize first letters
+            let track = track
+                .split('_')
+                .map(|word| {
+                    let mut chars = word.chars();
+                    match chars.next() {
+                        None => String::new(),
+                        Some(c) => c.to_uppercase().collect::<String>() + chars.as_str(),
+                    }
+                })
+                .collect::<Vec<_>>()
+                .join(" ");
+            let mut fastest_time = None;
+            let mut previous_time = None;
             let driver_laps = driver_laps
                 .into_iter()
-                .map(|(driver, laps)| (driver, laps[0]))
-                .sorted_by_key(|(_, bestlap)| *bestlap)
-                .map(|(driver, bestlap)| {
-                    let minutes = bestlap.as_secs() / 60;
-                    let seconds = bestlap.as_secs() % 60;
-                    let milliseconds = bestlap.subsec_millis();
-                    let formatted = format!("{:02}:{:02}.{:03}", minutes, seconds, milliseconds);
+                .sorted_by_key(|(_, lap)| lap.laptime)
+                .enumerate()
+                .map(|(position, (driver, lap))| {
                     let name = data.player_id_to_name.get(&driver).unwrap().clone();
-                    (name, formatted)
+                    let gap = match fastest_time {
+                        None => {
+                            fastest_time = Some(lap.laptime);
+                            "-".to_string()
+                        }
+                        Some(fastest) => format_duration(lap.laptime - fastest),
+                    };
+                    let interval = match previous_time {
+                        None => "-".to_string(),
+                        Some(previous) => format_duration(lap.laptime - previous),
+                    };
+                    previous_time = Some(lap.laptime);
+                    let splits = lap
+                        .splits
+                        .iter()
+                        .map(|split| format_duration(*split))
+                        .collect::<Vec<_>>()
+                        .join(" ");
+                    DisplayLine {
+                        player_id: driver,
+                        position: position + 1,
+                        name,
+                        laptime: format_duration(lap.laptime),
+                        gap,
+                        interval,
+                        splits,
+                        car: CAR_MODEL_ID_TO_NAME.get(&lap.car).unwrap().to_string(),
+                        date: lap.date.timestamp(),
+                    }
                 })
                 .collect::<Vec<_>>();
             (track, driver_laps)
@@ -321,7 +469,9 @@ async fn root() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .nest_service("/static", ServeDir::new("static"));
     let listener = TcpListener::bind("127.0.0.1:3000")
         .await
         .context(anyhow!("Failed to bind to port 3000"))?;
